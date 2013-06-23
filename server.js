28a90bb2999c1021698c96f9f5eb0c29bcadfff1
Ryan Arana <ryan.arana@gmail.com>
2013-06-23 11:47:15 -0700

    Rename date field to timestamp

--- server.js
+++ server.js
@@ -28,18 +28,6 @@ app.get('/api/:model.:format?', function(req, res) {
   }
 });
 
-// TODO: input verification!
-// TODO: ids
-app.post('/api/:model', function(req, res) {
-  var data = getData(req.params.model) || [];
-  console.log(req.body);
-  if (req.body) {
-    data.push(req.body);
-    jf.writeFileSync('./data/' + req.params.model + '.json', data);
-  }
-  return res.send(data[data.length-1]);
-});
-
 app.all('/api/clear/:model', function(req, res) {
   jf.readFile('./data/' + req.params.model + '.json', function(err, data) {
     if (err) {
@@ -49,6 +37,31 @@ app.all('/api/clear/:model', function(req, res) {
       return res.redirect('./api/' + req.params.model);
     }
   });
+});
+
+
+app.put('/api/:model/:id', function(req, res) {
+  var data = getData(req.params.model) || [];
+  for (var i=0; i<data.length; i++) {
+    var d = data[i];
+    if (d.timestamp === req.params.id) {
+      d = req.body;
+      data[i] = d;
+      jf.writeFileSync('./data/' + req.params.model + '.json', data);
+      return res.send(d);
+    }
+  }
+  return res.send(404, "No data found for " + req.params.id + ". Did you mean to POST instead?");
+});
+
+// TODO: input verification!
+app.post('/api/:model', function(req, res) {
+  var data = getData(req.params.model) || [];
+  if (req.body) {
+    data.push(req.body);
+    jf.writeFileSync('./data/' + req.params.model + '.json', data);
+  }
+  return res.send(data[data.length-1]);
 });
 
 app.get('/', function(req, res) {
